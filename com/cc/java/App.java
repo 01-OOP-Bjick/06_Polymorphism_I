29905c58cbbc97685ac8d1323594b630340c9c15
bjick84 <bjick@web.de>
2023-02-06 12:51:28 +0100

    letzter Stand

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -11,12 +11,12 @@ public class App {
         pollObj(queen);
         pollObj(worker);
         pollObj(drone);
-        
+
         // Das sollte eigentlich nicht gehen!
         HoneyBee bee = new HoneyBee();
         output(bee.doYourJob());
     }
-    // 2. Variante Polymorphie
+        // 2. Variante Polymorphie
     private static void pollObj(HoneyBee bee) {
         output(bee.doYourJob());
         output(bee.fly());
