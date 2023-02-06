2da0da65f70863f943af9761127fab36831e2fd7
bjick84 <bjick@web.de>
2023-02-06 10:35:48 +0100

    1. Variante Overloading

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,13 +8,29 @@ public class App {
         Worker worker = new Worker();
         Drone drone = new Drone();
         
-        output(queen.doYourJob());
-        output(worker.doYourJob());
-        output(drone.doYourJob());
+        pollObj(queen);
+        pollObj(worker);
+        pollObj(drone);
+        // output(queen.doYourJob());
+        // output(worker.doYourJob());
+        // output(drone.doYourJob());
         output("---------------------");
-        output(queen.fly());
-        output(worker.fly());
-        output(drone.fly());
+        // output(queen.fly());
+        // output(worker.fly());
+        // output(drone.fly());
+    }
+    // 1. Variante Overloading
+    private static void pollObj(Queen bee) {
+        output(bee.doYourJob());
+        output(bee.fly());
+    }
+    private static void pollObj(Worker bee) {
+        output(bee.doYourJob());
+        output(bee.fly());
+    }
+    private static void pollObj(Drone bee) {
+        output(bee.doYourJob());
+        output(bee.fly());
     }
 
     private static void output(String outputStr) {
